2b415ceda434e5442da9bc5f59d0fdb0c7c4c327
Prajwal-PL <prajwalpl096@gmail.com>
2024-11-25 18:56:06 +0530

    Adding role validation from token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,11 @@
-const Token = require('../utils/token')
-const { UnauthorizedError } = require('../utils/errors')
+const Token = require("../utils/token");
+const { UnauthorizedError } = require("../utils/errors");
 
 // Middleware to check if the user is authenticated
 module.exports = async (req, res, next) => {
   const token = new Token();
 
   const authToken = req.headers["authorization"]?.split(" ")[1];
-
 
   if (!authToken)
     throw new UnauthorizedError("Authentication token is required");
@@ -18,6 +17,15 @@ module.exports = async (req, res, next) => {
       throw new Error("Invalid Authentication token");
     }
     req.userId = decodedToken.sub;
+
+    if (!req.userId) {
+      throw new UnauthorizedError("Invalid user");
+    }
+
+    if (decodedToken.role !== "recruiter") {
+      throw new Error("Invalid role");
+    }
+
     next();
   } catch (error) {
     throw new UnauthorizedError(error.message);
